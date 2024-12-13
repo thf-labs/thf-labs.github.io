ac2f76d76d669366dfae98fb22d9fd12c2be42f7
wigggles <mrwiggglez@gmail.com>
2024-12-13 10:37:14 -0600

    Create example react web app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          THF Component Lib Example App.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
